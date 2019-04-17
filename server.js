c8d19d4838be27ed6832c49a9775de841dfa744b
John Oro <johnmichaeloro@gmail.com>
2019-04-17 15:42:51 -0600

    Commit 3 - index.ejs view rendered at pokemon route

--- server.js
+++ server.js
@@ -6,10 +6,16 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended:false}));
 
+app.use(express.static('css'));
+
+//Once I declare a static file, I only need to link to the file itself in the html style link. In my html, I have <link rel="stylesheet" href="style.css">. No need to include a file path as the file path has been declared above. Usually, a public file is created in which the static files are stored. So it should be read _css + '/public' in the parameter above. But since I only have a css folder, I haven't included this additional information.
+
 //This requires a function that takes in an export path. In the pokemon.js file, I've added at the bottom a module.exports equals and the varaible name.
 
 app.get('/pokemon', (req, res) => {
-  res.send(pokemon)
+  res.render('index.ejs', {
+    'pokemon': pokemon
+  })
 })
 
 app.listen(port, () => {
