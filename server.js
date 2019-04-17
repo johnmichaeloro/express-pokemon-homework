67f3dfd190079f70ee8dbbc6e4269b8f3ec60eaa
John Oro <johnmichaeloro@gmail.com>
2019-04-17 15:00:05 -0600

    connected my database, can see json in the browser

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const pokemon = require("./pokemon");
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res) => {
-  res.send('Server is running... away?')
+app.use(bodyParser.urlencoded({extended:false}));
+
+//This requires a function that takes in an export path. In the pokemon.js file, I've added at the bottom a module.exports equals and the varaible name.
+
+app.get('/pokemon', (req, res) => {
+  res.send(pokemon)
 })
 
 app.listen(port, () => {
