4b1a36fd31a44c837f4857a85e0a15e00b731d5a
John Oro <johnmichaeloro@gmail.com>
2019-04-18 13:36:49 -0600

    Pokemon CRUD App working

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express();
 const port = 3000;
 const pokemon = require("./pokemon");
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override')
+
+app.use(methodOverride('_method'));
 
 app.use(bodyParser.urlencoded({extended:false}));
 
@@ -18,12 +21,48 @@ app.get('/pokemon', (req, res) => {
   })
 })
 
+app.get('/pokemon/new', (req, res) => {
+  res.render('new.ejs')
+})
+
+app.post('/pokemon', (req, res) => {
+  pokemon.push(req.body);
+  res.redirect('/pokemon')
+})
+
 app.get('/pokemon/:id', (req, res) => {
   res.render('show.ejs', {
-    'pokemon': pokemon[req.params.id]
+    'pokemon': pokemon[req.params.id],
+    'id': req.params.id
   })
 })
+
+//I need to build an edit route and a render route
+
+app.get('/pokemon/:id/edit', (req, res) => {
+  res.render('edit.ejs', {
+    'pokemon': pokemon[req.params.id],
+    'id': req.params.id
+  })
+})
+
+app.delete('/pokemon/:id', (req, res) => {
+  res.redirect('/pokemon');
+  pokemon.splice(req.params.id, 1)
+})
+
+app.put('/pokemon/:id', (req, res) => {
+  pokemon[req.params.id] = req.body;
+  res.redirect('/pokemon')
+})
+
+//End of suspect routes
 
 app.listen(port, () => {
   console.log('app listening on port' + port);
 })
+
+//Users should be able to insert a new pokemon into the array using a form on a `new.ejs` page. Creation should be handled via a POST route to the `/pokemon` route.
+//Users should be able to edit an individual pokemon on an `edit.ejs` page accessed from the  `/pokemon/:id/edit` route. The updating should be handled via a POST request to the `/pokemon/:id` route.
+//Users should be able to delete a pokemon using a button provided on the show and index pages.
+//The final app should have what are known as the 7 RESTful routes.
